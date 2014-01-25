13b760f228caa2d74800bef9dd122331d5ccacb7
papri <papriwalprateek@gmail.com>
2014-01-25 18:56:56 +0530

    recreate algoboot

--- simplecatextract.rb
+++ simplecatextract.rb
@@ -1,13 +1,18 @@
-load "algoboot.rb"
+load "~/dqscripts/algoboot.rb"
 @cats["Graph algorithms"] = []
 
+@doc = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/List_of_algorithms"))
 @node = @doc.css("span#Graph_algorithms")[0].parent
 @stop = @doc.css("span#Sequence_algorithms")[0].parent
 
 while @node != @stop
 	@node = @node.next
 	begin
-		@node.css("li > a").map {|a| @cats["Graph algorithms"] << a["title"]}
+		@node.css("li > a").each do |a| 
+			if !@cats["Graph algorithms"].include?(a["title"])
+				@cats["Graph algorithms"] << a["title"]
+			end
+		end
 	end
 end
 
