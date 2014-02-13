b19a61a5a11f47b7ae53d09c21114b33582b34a8
papriwalprateek <papriwalprateek@gmail.com>
2014-02-13 15:48:03 +0530

    sort algos description

---
+++ desc.rb
@@ -0,0 +1,51 @@
+load '/home/papri/Downloads/git_work/dqscripts/svm/corpus.rb'
+
+def nist_extract(url)
+	@doc = Nokogiri::HTML(open(url))
+	carrier = @doc.css('p')
+	
+	desc = ''
+	carrier.each do |c|
+		if c.text.include?('Definition')
+			x = c.css('strong')
+			x.remove
+			desc = c.text
+			break
+		end
+	end
+	return desc
+end
+
+def wolfram_extract(url)
+	@doc = Nokogiri::HTML(open(url))
+	carrier = @doc.css('p')
+	desc = ''
+	carrier.each do |c|
+		if desc.length < 200
+			desc = desc + c.text
+		else
+			break
+		end
+	end
+	return desc
+end
+
+
+
+@sortcorpus = @corpus['Sequence sorting']
+
+@sortdesc = {}
+
+@sortcorpus.each do |a,v|
+	@sortdesc[a] = {}
+	puts a
+	v.each do |t|
+		if t[1].include?('xlinux.nist.gov')
+			@sortdesc[a]['xlinux.nist.gov'] = nist_extract(t[1])
+			puts t[1]
+		elsif t[1].include?('wolfram')
+			@sortdesc[a]['wolfram'] = wolfram_extract(t[1])
+			puts t[1]
+		end 
+	end
+end
