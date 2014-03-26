7d4a742e9810073c38245d02ab44a4fb173b10fb
Prateek Papriwal <papriwalprateek@gmail.com>
2014-03-26 10:45:24 +0530

    machine learning algos

--- simplecatextract.rb
+++ simplecatextract.rb
@@ -1,19 +1,20 @@
-load "/home/papri/Downloads/git_work/dqscripts/algoboot.rb"
-@cats["Trees (data structure)"] = []
+load "/home/papri/git_work/dqscripts/algoboot.rb"
+@cats["Machine learning algorithms"] = []
 
-@doc = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/List_of_data_structures"))
-@node = @doc.css("span#Trees")[0].parent
-@stop = @doc.css("span#Hashes")[0].parent
+@doc = Nokogiri::HTML(open("http://en.wikipedia.org/wiki/List_of_algorithms"))
+@node = @doc.css("span#Machine_learning_and_statistical_classification")[0].parent
+@stop = @doc.css("span#Programming_language_theory")[0].parent
 
 while @node != @stop
 	@node = @node.next
 	begin
-		@node.css("li > a").each do |a| 
-			if !@cats["Trees (data structure)"].include?(a["title"])
-				@cats["Trees (data structure)"] << a["title"]
-				puts a['title']
+			@node.css("li").each do |l| 
+			if !@cats["Machine learning algorithms"].include?(l.css('a')[0]['title'])
+				@cats["Machine learning algorithms"] << l.css('a')[0]['title']
+				puts l.css('a')[0]['title']
 			end
-		end
+			end
+	rescue
 	end
 end
 
