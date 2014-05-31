ecd9c786f31fb0951e7351fb903bef82f32b5c65
papriwalprateek <papriwalprateek@gmail.com>
2014-06-01 00:25:03 +0530

    techopedia and webopedia added for all 3 cats

--- oscorpus/addtocorpus.rb
+++ oscorpus/addtocorpus.rb
@@ -1,32 +1,42 @@
 load '/home/papri/git_work/dqscripts/oscorpus/oscorpus.rb'
 load '/home/papri/git_work/dqscripts/oscorpus/corpus.rb'
 
-def desccorpus
-	@corpus['Basic Concepts'].each do |t|
+def corpus_init(cat)
+	@oscorpus[cat] = {}
+	@corpus[cat].each do |t|
+		@oscorpus[cat][t[0]] = {}		
+	end
+	desccorpus(cat)
+	techopedia(cat)
+end
+
+def desccorpus(cat)
+
+	@corpus[cat].each do |t|
 		puts t[0]
-		@oscorpus['Basic Concepts'][t[0]]['desc'] = []
+		@oscorpus[cat][t[0]]['desc'] = []
 		puts t[0]
 		t[1].each do |u|
 			if u[1].include?('webopedia')
-				@oscorpus['Basic Concepts'][t[0]]['desc'] << u[1]
+				@oscorpus[cat][t[0]]['desc'] << u[1]
 				puts u[1]
 				break
 			end
 		puts "hello"
 		end
 	end
+	File.open('/home/papri/git_work/dqscripts/oscorpus/oscorpus.rb','w'){|file| file.write("# encoding: UTF-8" + "\n\n@oscorpus = " + @oscorpus.to_s)}
 end
 
-def techopedia
+def techopedia(cat)
 	load '/home/papri/git_work/dqscripts/oscorpus/oscorpus.rb'
-	@corpus['Basic Concepts'].each do |t|
+	@corpus[cat].each do |t|
 		Google::Search::Web.new(:query => t[0] + "techopedia").each do |w|
 			if w.uri.include?('techopedia')
-				@oscorpus['Basic Concepts'][t[0]]['desc'] << w.uri
+				@oscorpus[cat][t[0]]['desc'] << w.uri
 				puts w.uri
 				break
 			end
 		end
 	end
 end
-
